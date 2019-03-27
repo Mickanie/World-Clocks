d70b8bbd44a6058a9fb9f665f608f79de905e14c
Mickanie <michalina.kaniewska@gmail.com>
2019-03-28 00:29:09 +0100

    Updates

---
+++ precache-manifest.592166393e3a0218481d00edb9aa902e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "08381d4be7612ca092e9d1c7a2db622e",
+    "url": "/World-Clocks/static/media/earth.08381d4b.png"
+  },
+  {
+    "revision": "63f2287eedbfadbeddd41ca3105947a2",
+    "url": "/World-Clocks/static/media/clockface.63f2287e.png"
+  },
+  {
+    "revision": "d1b610f002eabc90c510",
+    "url": "/World-Clocks/static/js/runtime~main.d1b610f0.js"
+  },
+  {
+    "revision": "65e822a8f6d78e224757",
+    "url": "/World-Clocks/static/js/main.65e822a8.chunk.js"
+  },
+  {
+    "revision": "3b0179b9f75c503e7eb5",
+    "url": "/World-Clocks/static/js/1.3b0179b9.chunk.js"
+  },
+  {
+    "revision": "65e822a8f6d78e224757",
+    "url": "/World-Clocks/static/css/main.1404f50f.chunk.css"
+  },
+  {
+    "revision": "9ca1db807a6aef7ef9151de5dc1c5924",
+    "url": "/World-Clocks/index.html"
+  }
+];
